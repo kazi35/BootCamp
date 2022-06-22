ed08597b6866bf57f0491e34034e6a07afcebe60
Md Nashir Kazi <99824485+kazi35@users.noreply.github.com>
2022-06-22 10:36:51 +0600

    Add files via upload

---
+++ condition.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{ 
+   int a,b,c,sum;
+   printf("Enter the valu of a:\n");
+	scanf("%d",&a);
+	 printf("Enter the valu of b:\n");
+	  	scanf("%d",&b);
+	   printf("Enter the valu of c:\n");	
+		scanf("%d",&c);
+	sum=a+b+c;
+	if(sum==180)
+    {
+    	printf("The Triangle is valid");
+    	
+    	
+    	
+		}	
+	else{
+		printf("The triangle is not valid");
+	}
+	
+	
+	return 0;
+}
